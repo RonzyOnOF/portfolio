cec07e1e9dac0cedae0dee73c139d1c8e52ada46
JohnnyBoi <ninjakai25@gmail.com>
2023-07-06 21:05:58 -0500

    add hamburger menu when screen size is smaller and work on responsive design for home page

--- src/components/coverPage/CoverPage.jsx
+++ src/components/coverPage/CoverPage.jsx
@@ -1,16 +1,15 @@
 import { Typewriter } from 'react-simple-typewriter'
 import styles from './CoverPage.module.css';
-import me from '../../../public/images/portme.png';
+import me from '../../../public/images/portfolioMe.png';
 
 export const CoverPage = () => {
     return (
         <>
             <div className={styles.coverText}>
                 <div className={styles.text}>
-                    <h2>Home Page</h2>
-                    <p>Hello, I'm</p>
-                    <p>Jonathan Santos</p>
-                    <p>A<Typewriter 
+                    <h3>Hello, I'm</h3>
+                    <p id={styles.name}>Jonathan Santos</p>
+                    <p id={styles.typewriter}>A<Typewriter 
                         words={[' Web Content Editor', ' Web Developer']}
                         loop={0 | false}
                         deleteSpeed={100}
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,16 +1,48 @@
 import styles from './Navbar.module.css';
-import logo from '../../../public/images/jsDev2.png';
+import { useState, useEffect } from 'react';
+import { motion } from 'framer-motion';
 
 export const Navbar = () => {
+    
+    const [isClicked, setIsClicked] = useState();
+    const [width, setWidth] = useState();
+
+    const toggleClick = () => {
+        setIsClicked(!isClicked);
+    }
+
+    const getWidth = () => {
+        return window.innerWidth;
+    }
+
+    useEffect(() => {
+        function handleResize() {
+            setWidth(getWidth());
+          }
+        window.addEventListener('resize', handleResize);
+        return () => window.removeEventListener('resize', handleResize);
+    }, [])
+
+    if(isClicked && width >= 880) {
+        setIsClicked(false);
+    }
+    
+    
     return(
         <>
             <nav className={styles.navBar}>
                 <h1 id={styles.name}>JSDev</h1>
-                <ul className={styles.list}>
+                <ul className={isClicked ? styles.listActive : styles.list}>
                     <a><li>Home</li></a>
                     <a><li>About</li></a>
+                    <a><li>Projects</li></a>
                     <a><li>Contact</li></a>
                 </ul>
+                <a className={styles.toggleButton} onClick={toggleClick}>
+                    <span className={styles.bar}></span>
+                    <span className={styles.bar}></span>
+                    <span className={styles.bar}></span>
+                </a>
             </nav>
         </>
     )
